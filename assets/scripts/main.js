b89762d0f51e8bc41df21f042c83e14adc8b787c
Constantine Shvetsov <cnstntn@mail.ru>
2021-07-28 20:12:41 +0300

    code cleaning

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,76 +8,6 @@ $(document).ready(function () {
     new jBox('Image');
 });
 
-
-// Кнопка наверх:
-$(document).ready(function () {
-    // $(window).scroll(function () {
-    //     if ($(window).scrollTop() > 200) {
-    //         $('.back-to-top__button').fadeIn();
-    //     } else {
-    //         $('.back-to-top__button').fadeOut();
-    //     }
-    // });
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 200) {
-            $('.back-to-top__button').addClass('show');
-        } else {
-            $('.back-to-top__button').removeClass('show');
-        }
-    });
-
-    $('.back-to-top__button').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
-});
-
-// Выпадающее меню:
-$(document).ready(function () {
-    $('.dropdown').click(function () {
-        $(this).toggleClass('active');
-        $(this).find('.dropdown__menu').slideToggle();
-    });
-});
-
-// Выпадающее меню (закрыть по клику вне окна):
-$(document).mouseup(function (e) {
-    var dropdownActive = $('.dropdown.active');
-    if (e.target != dropdownActive[0] && dropdownActive.has(e.target).length === 0) {
-        $('.dropdown.active').removeClass('active').find('.dropdown__menu').slideUp();
-    }
-});
-
-
-// Yandex Map
-$(document).ready(function () {
-    $('.map__overlay').click(function () {
-        $('.map').addClass('active');
-    });
-
-    $('.map iframe').mouseleave(function () {
-        $('.map').removeClass('active');
-    });
-});
-
-// Счетчики
-$(document).ready(function () {
-    $('.counter').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 2500,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-});
-
 // Гамбургер (кнопка меню):
 $(document).ready(function () {
     $('.header__toggler').click(function () {
@@ -93,26 +23,7 @@ $(window).resize(function () {
     }
 }).resize();
 
-// Модальное окно
-$(document).ready(function () {
-    $('.callback__button').click(function () {
-        $('.modal-callback').fadeIn();
-    });
-
-    $('.advice__button').click(function () {
-        $('.modal-advice').fadeIn();
-    });
-
-    $('.form-submit').click(function () {
-        $('.modal').delay(500).fadeOut();
-    });
-
-    $('.close-button').click(function () {
-        $('.modal').fadeOut();
-    });
-});
-
-// Slick Слайдеры:
+// Slick Слайдер:
 $(document).ready(function () {
     $('.main__slider').slick({
         autoplay: true,
